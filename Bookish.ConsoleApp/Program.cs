995179b9c3dc4c7283b1bd20afe29c5c1a5fbef6
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-23 17:05:31 +0000

    Console application to view books

--- Bookish.ConsoleApp/Program.cs
+++ Bookish.ConsoleApp/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Bookish.DataAccess;
 
 namespace Bookish.ConsoleApp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+      var repo = new BookRepository();
+      var books = repo.GetAllBooks();
+
+      foreach (var book in books)
+      {
+        Console.WriteLine($"{book.Title} by {book.Author}");
+      }
+
+      Console.ReadLine();
     }
+  }
 }
---
+++ Bookish.DataAccess/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Bookish.DataAccess
+{
+  public class BookRepository
+  {
+    public IEnumerable<Book> GetAllBooks()
+    {
+      using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["MySqlServer"].ConnectionString))
+      {
+        return db.Query<Book>("SELECT * FROM Books");
+      }
+    }
+  }
+}
