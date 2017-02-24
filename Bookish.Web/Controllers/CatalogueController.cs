ac43390c548b2818f50eda9d8d1a3c5accbe3e80
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-24 14:16:45 +0000

    Unpaged library catalogue

---
+++ Bookish.Web/Controllers/CatalogueController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookish.DataAccess;
+
+namespace Bookish.Web.Controllers
+{
+  public class CatalogueController : Controller
+  {
+    private readonly BookRepository bookRepository = new BookRepository();
+
+    public ActionResult Index()
+    {
+      var books = bookRepository.GetAllBooks();
+
+      return View(books);
+    }
+  }
+}
