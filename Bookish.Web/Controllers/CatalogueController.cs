ffdf0d087a9bd0245592ac35b73c6ecbb44fd216
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-24 14:38:49 +0000

    Paged catalogue view

--- Bookish.Web/Controllers/CatalogueController.cs
+++ Bookish.Web/Controllers/CatalogueController.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Bookish.DataAccess;
+using Bookish.Web.Models;
 
 namespace Bookish.Web.Controllers
 {
   public class CatalogueController : Controller
   {
     private readonly BookRepository bookRepository = new BookRepository();
+    private const int PageSize = 20;
 
-    public ActionResult Index()
+    public ActionResult Index(int page = 1)
     {
-      var books = bookRepository.GetAllBooks();
+      var allBooks = bookRepository.GetAllBooks().OrderBy(book => book.Title);
+      var pageOfBooks = allBooks.Skip((page - 1)*PageSize).Take(PageSize);
+      var totalPages = (int)Math.Ceiling(allBooks.Count()/(double)PageSize);
 
-      return View(books);
+      return View(new CatalogueViewModel(page, totalPages, pageOfBooks));
     }
   }
 }
