8bf86dfec71419ecb2a8eceacecef96c99ecf7fb
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-27 15:34:21 +0000

    Add test that fails due to lack of SQL connection

--- Bookish.Web.Tests/Controllers/CatalogueControllerTests.cs
+++ Bookish.Web.Tests/Controllers/CatalogueControllerTests.cs
@@ -1,9 +1,23 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Bookish.Web.Controllers;
+using Bookish.Web.Models;
+using NUnit.Framework;
 
 namespace Bookish.Web.Tests.Controllers
 {
   [TestFixture]
   public class CatalogueControllerTests
   {
+    [Test]
+    public void Index_ReturnsOnePage()
+    {
+      var controller = new CatalogueController();
+
+      var viewResult = (ViewResult)controller.Index();
+
+      var model = (CatalogueViewModel)viewResult.Model;
+      Assert.LessOrEqual(20, model.Books.Count());
+    }
   }
 }
